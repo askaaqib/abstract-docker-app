6569d5f79b9be2981e041b3d8ffdf4e14ce13c4f
Aqib <askaaqib@gmail.com>
2020-10-08 11:05:13 +0500

    get method test

--- app/admin/src/Controllers/Test/Test.php
+++ app/admin/src/Controllers/Test/Test.php
@@ -24,4 +24,21 @@ class Add extends AbstractAdminController {
 //        Schema::Bind($db, 'App\Common\Database\Primary\Users');
     }
 
+    public function get()
+    {
+        $this->page()->title('Create Test')->index(610, 20)
+            ->prop("icon", "mdi mdi-account-plus-outline");
+
+        $this->breadcrumbs("Test Control", null, "ion ion-ios-people");
+        $template = $this->template("/test/add.knit");
+        //   ->assign("form", $form->array());
+        $this->body($template);
+        // Response
+        /*   $this->response()->set("status", true);
+           $this->response()->set("disabled", true);
+           $this->messages()->success('You have been logged in!');
+           $this->messages()->info('Please wait...');
+           $this->response()->set("redirect", $this->request()->url()->root($authToken->base16()->hexits() . "/dashboard"));
+           return;*/
+    }
 }
